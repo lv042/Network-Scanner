eff1308ce6ccf36b1ff48693d32bee921147e8ed
Luca v. K <luca.v.kannen@gmail.com>
2022-12-27 00:12:37 +0100

    Made some fine tuning

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from pythonping import ping
 import time
 import concurrent.futures
 
-#ping a list of hosts in parallel with a timeout of 0.25 seconds, a packet loss threshold of 10%, and a count of 20
+ScanClassB = False
 timeout = 0.25
-packet_loss_threshold = 10
-count = 20
+packet_loss_threshold = 1
+count = 3
+num_threads = 100
 
 
 def main():
@@ -26,13 +27,18 @@ def main():
 
     split = ip.split('.')
     
-    for i in range(1, 256):
+    if not ScanClassB:
         for j in range(1, 256):
-            new_ip = split[0] + '.' + split[1] + '.' + str(i) + '.' + str(j)
+            new_ip = split[0] + '.' + split[1] + '.' + split[2] + '.' + str(j)
             hosts.append(new_ip)
+    else:
+        for i in range(1, 256):
+            for j in range(1, 256):
+                new_ip = split[0] + '.' + split[1] + '.' + str(i) + '.' + str(j)
+                hosts.append(new_ip)
 
     # print(hosts)
-    ping_hosts_in_parallel(hosts, 0.25)
+    ping_hosts_in_parallel(hosts, timeout, packet_loss_threshold, count)
 
     print("Responders: " + str(responders) + "\n")
 
@@ -45,17 +51,20 @@ def ping_host(host, timeout, packet_loss_threshold, count):
     #ping the host with the specified timeout and packet loss threshold
     ping_result = ping(target=host, count=count, timeout=timeout)
     print("Pinging: " + host + "\n")
-    print(ping_result)
+    #print all attributes of the ping result
+    print(ping_result.__dict__)
 
     #add host to responders list if packet loss is below the specified threshold
-    if ping_result.packet_loss <= packet_loss_threshold:
+    if ping_result.packet_loss < packet_loss_threshold:
         print("Host is up: " + host + "\n")
         responders.append(host)
     else:
         print("Host is down: " + host + "\n")
 
+    
+
 def ping_hosts_in_parallel(hosts, timeout, packet_loss_threshold, count):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+      with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         # Start the ping processes (in this case, using a thread pool)
         results = [executor.submit(ping_host, host, timeout, packet_loss_threshold, count) for host in hosts]
 
